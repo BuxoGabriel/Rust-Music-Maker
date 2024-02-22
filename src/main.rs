1834d5a45c1d17f6e8f1760a866614e4d058fd5d
Gabriel Buxo <gabrielbuxo1@gmail.com>
2024-02-22 01:31:24 -0500

    Added ability to save and load song data from a custom file type

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{f32::consts::PI, fs::File, io::{self, Write}};
+use std::{f32::consts::PI, fs::File, io::{self, BufReader, Read, Write}};
+use rfd::FileDialog;
 
 const BITS_PER_SAMPLE: u16 = 16;
 const MAX_AMPLITUDE: i16 = i16::MAX;
@@ -63,9 +64,9 @@ struct Note {
 }
 
 impl Note {
-    fn new(time: f32, duration: f32, frequency: f32, volume: f32) -> Result<Self, String> {
+    fn new(time: f32, duration: f32, frequency: f32, volume: f32) -> Result<Self, &'static str> {
         if volume > 1.0 {
-            return Err("Note must have volume in range [0, 1]".to_string());
+            return Err("Note must have volume in range [0, 1]");
         }
         Ok(Note {time, duration, frequency, volume})
     }
@@ -88,12 +89,13 @@ impl Note {
 
 /// Represents a musical instrement or part. Can only play one [Note] at a time and multiple Parts are part of a [Song]
 struct Part {
+    name: String,
     notes: Vec<Note>
 }
 
 impl Part {
-    fn new() -> Self {
-        Part { notes: Vec::new()}
+    fn new(name: String) -> Self {
+        Part { name, notes: Vec::new()}
     }
 
     // Checks if the part has a note at a certain time
@@ -106,10 +108,10 @@ impl Part {
         None
     }
 
-    fn add_note(&mut self, note: Note) -> Result<(), String>{
+    fn add_note(&mut self, note: Note) -> Result<(), &'static str>{
         for note_i in &self.notes {
             if note_i.plays_at(note.time) || note_i.plays_at(note.end_time()) {
-                return Err("can't add note inside another notes play time".to_string());
+                return Err("can't add note inside another notes play time");
             }
         }
         self.notes.push(note);
@@ -130,7 +132,9 @@ impl Part {
 
 impl Default for Part {
     fn default() -> Self {
-        Part { notes: vec![
+        Part { 
+            name: "Melody".to_string(),
+            notes: vec![
             Note { frequency: 440.0 /* A */, volume: 0.25, time: 0.0, duration: 1.0 },
             Note {frequency: 440.0 /* A */, volume: 0.5, time: 2.0, duration: 1.0 },
             Note {frequency: 293.99, volume: 0.5, time: 3.0, duration: 1.0 }]
@@ -154,6 +158,7 @@ impl Default for WavOptions {
     }
 }
 
+
 struct Song {
     name: String,
     parts: Vec<Part>
@@ -161,13 +166,9 @@ struct Song {
 
 impl Song {
     fn new(name: String) -> Self {
-        Song { name, parts: vec![
-            Part::default(),
-            Part { notes: vec![
-                Note { frequency: 293.99, volume: 0.25, time: 0.0, duration: 1.0 }, 
-                Note { frequency: 293.99, volume: 0.25, time: 1.0, duration: 0.5 },
-                Note { frequency: 150.00, volume: 0.25, time: 1.5, duration: 1.5 }] 
-            }]
+        Self {
+            name,
+            parts: Vec::new()
         }
     }
 
@@ -211,7 +212,7 @@ impl Song {
         bytes
     }
 
-    fn write_to_file(&self, file_name: &str, options: &WavOptions) {
+    fn write_to_wav_file(&self, file_name: String, options: &WavOptions) {
         let mut file_name = String::from(file_name);
         file_name.push_str(".wav");
         println!("Writing to file {file_name}!");
@@ -224,6 +225,227 @@ impl Song {
 
         let _ = file.write_all(&self.compile_parts_into_bytes(options));
     }
+
+    fn write_to_song_file(&self, mut file_name: String) {
+        file_name.push_str(".song");
+        println!("Writing to file {file_name}!");
+        let mut file = File::create(file_name.as_str()).expect("Failed to create file");
+        match &self.serialize() {
+            Ok(serialized_data) => {
+                file.write_all(&serialized_data);
+            }
+            Err(err) => {
+                println!("{err}");
+            }
+        }
+    }
+}
+
+impl Default for Song {
+    fn default() -> Self {
+        Song { name: "Demo Song".to_string(), parts: vec![
+            Part::default(),
+            Part { name: "base".to_string(), notes: vec![
+                Note { frequency: 293.99, volume: 0.25, time: 0.0, duration: 1.0 }, 
+                Note { frequency: 293.99, volume: 0.25, time: 1.0, duration: 0.5 },
+                Note { frequency: 150.00, volume: 0.25, time: 1.5, duration: 1.5 }] 
+            }]
+        }
+    }
+}
+
+trait Serializable {
+    fn serialize(&self) -> Result<Vec<u8>, &'static str>;
+    fn deserialize(serialized_data: &[u8]) -> Result<Self, &'static str>
+    where 
+        Self: Sized;
+}
+
+impl Serializable for Note {
+    /// Serializes a `Note` struct into a byte representation
+    /// f32: time
+    /// f32: duration
+    /// f32: frequency
+    /// f32: volume
+    fn serialize(&self) -> Result<Vec<u8>, &'static str> {
+        let mut serialized_data = Vec::new();
+        // Serialize the time
+        let time_bytes = self.time.to_le_bytes();
+        serialized_data.extend(time_bytes);
+        // Serialize the duration
+        let dur_bytes = self.duration.to_le_bytes();
+        serialized_data.extend(dur_bytes);
+        // Serialize the frequency
+        let freq_bytes = self.frequency.to_le_bytes();
+        serialized_data.extend(freq_bytes);
+        // Serialize the volume
+        let vol_bytes = self.volume.to_le_bytes();
+        serialized_data.extend(vol_bytes);
+        Ok(serialized_data)
+    }
+
+    fn deserialize(serialized_data: &[u8]) -> Result<Self, &'static str> {
+        if serialized_data.len() != 16 {
+            return Err("Invalid serialized data! Insuffient data for note");
+        }
+        // Deserialize the time
+        let time_bytes = &serialized_data[0..4];
+        let time = f32::from_le_bytes(time_bytes.try_into().unwrap());
+        // Deserialize the duration
+        let dur_bytes = &serialized_data[4..8];
+        let duration = f32::from_le_bytes(dur_bytes.try_into().unwrap());
+        // Deserialize the frequency
+        let freq_bytes = &serialized_data[8..12];
+        let frequency = f32::from_le_bytes(freq_bytes.try_into().unwrap());
+        // Deserialize the volume
+        let vol_bytes = &serialized_data[12..16];
+        let volume = f32::from_le_bytes(vol_bytes.try_into().unwrap());
+        Ok(Self { time, duration, frequency, volume })
+    }
+}
+
+impl Serializable for Part {
+    /// Serializes a `Part` struct into a byte representation
+    /// u16: name_len
+    /// name_len: name
+    /// u16: num_notes
+    /// (notes)
+    fn serialize(&self) -> Result<Vec<u8>, &'static str> {
+        let mut serialized_data = Vec::new();
+        // Serialize the name
+        let name_as_bytes = self.name.as_bytes();
+        let name_len = name_as_bytes.len();
+        if name_len > u16::MAX as usize {
+            return Err("Could not serialize part. Name too long!");
+        }
+        let name_len = name_len as u16;
+        serialized_data.extend(name_len.to_le_bytes());
+        serialized_data.extend(name_as_bytes);
+        // Serialize number of notes
+        let num_notes = self.notes.len() as u16;
+        serialized_data.extend(num_notes.to_le_bytes());
+        // Serialize each note
+        for note in &self.notes {
+            match note.serialize() {
+                Ok(serialized_note) => {
+                    serialized_data.extend(serialized_note)
+                }
+                Err(err) => {
+                    return Err(err);
+                }
+            }
+        }
+        Ok(serialized_data)
+    }
+
+    fn deserialize(serialized_data: &[u8]) -> Result<Self, &'static str> {
+        // Deserialize name
+        if serialized_data.len() < 2 {
+            return Err("Invalid serialized data! Insufficient length for part name size!");
+        }
+        let name_len_bytes = &serialized_data[..2];
+        let name_len = u16::from_le_bytes(name_len_bytes.try_into().unwrap()) as usize;
+        if serialized_data.len() < 2 + name_len {
+            return Err("Invalid serialized data! Insufficient length for part name");
+        }
+        let name_bytes = &serialized_data[2..(2+name_len as usize)];
+        let name = String::from_utf8_lossy(name_bytes).into_owned();
+        // Deserialize number of notes
+        if (&serialized_data[(2+name_len as usize)..] as &[u8]).len() < 2 {
+            return Err("Invalid serialized data! Insufficent length for number of notes!");
+        }
+        let num_notes_bytes = &serialized_data[(2+name_len)..(4+name_len)];
+        let num_notes = u16::from_le_bytes(num_notes_bytes.try_into().unwrap());
+        // Deserialize individual notes
+        let mut remaining_bytes: &[u8] = &serialized_data[(4+name_len)..];
+        let mut notes = Vec::new();
+        for _ in 0..num_notes {
+            if remaining_bytes.len() < 2 {
+                return Err("Invalid serialized data! Insufficient length for note size");
+            }
+            let note_bytes = &remaining_bytes[..16];
+            notes.push(Note::deserialize(note_bytes)?);
+            remaining_bytes = &remaining_bytes[16..];
+        }
+        Ok(Self { name, notes })
+    }
+}
+
+impl Serializable for Song {
+    /// Serializes a `Song` struct into a byte representation
+    /// u16: name_len
+    /// name_len: name
+    /// u16: num parts
+    /// (parts) u16: size_of_part
+    /// (parts) size_of_part: part
+    fn serialize(&self) -> Result<Vec<u8>, &'static str> {
+        let mut serialized_data: Vec<u8> = Vec::new();
+        // Serialize the name
+        let name_as_bytes = self.name.as_bytes();
+        let name_len = name_as_bytes.len();
+        if name_len > u16::MAX as usize {
+            return Err("Could not serialize song. Name too long!");
+        }
+        let name_len = name_len as u16;
+        serialized_data.extend(name_len.to_le_bytes());
+        serialized_data.extend(name_as_bytes);
+        // Serialize number of parts
+        let num_parts: u16 = self.parts.len() as u16;
+        serialized_data.extend(num_parts.to_le_bytes());
+        // Serialize each part
+        for part in &self.parts {
+            match part.serialize() {
+                Ok(serialized_part) => {
+                    let size_of_part = serialized_part.len() as u16;
+                    serialized_data.extend(size_of_part.to_le_bytes());
+                    serialized_data.extend(serialized_part);
+                }
+                Err(err) => {
+                    return Err(err);
+                }
+            }
+        }
+        Ok(serialized_data)
+    }
+
+    fn deserialize(serialized_data: &[u8]) -> Result<Self, &'static str> {
+        if serialized_data.len() < 2 {
+            return Err("Invalid serialized data! Insufficient length for song name size!");
+        }
+        // Deserialize name
+        let name_len_bytes = &serialized_data[..2];
+        let name_len = u16::from_le_bytes(name_len_bytes.try_into().unwrap()) as usize;
+        if serialized_data.len() < 2 + name_len {
+            return Err("Invalid serialized data! Insufficent length for song name!");
+        }
+        let name_bytes = &serialized_data[2..(2+name_len)];
+        let name = String::from_utf8_lossy(name_bytes).into_owned();
+        // Deserialize number of parts
+        if (&serialized_data[(2+name_len)..] as &[u8]).len() < 2 {
+            return Err("Invalid serialized data! Insufficent length for number of parts!");
+        }
+        let num_parts_bytes = &serialized_data[(2+name_len)..(4+name_len)];
+        let num_parts = u16::from_le_bytes(num_parts_bytes.try_into().unwrap());
+        // Deserialize parts        
+        let mut remaining_data = &serialized_data[(4+name_len)..];
+        let mut parts = Vec::new();
+        for _ in 0..num_parts {
+            if remaining_data.len() < 2 {
+                return Err("Invalid serialized data! Missing part length data!");
+            }
+            // Deserialize part len
+            let part_size_bytes: &[u8] = &remaining_data[..2];
+            let part_size = u16::from_le_bytes(part_size_bytes.try_into().unwrap()) as usize;
+            // Deserialize part
+            if remaining_data.len() < 2 + part_size {
+                return Err("Invalid serialized data! Missing part data!");
+            } 
+            let part_bytes = &remaining_data[2..(2+part_size)];
+            parts.push(Part::deserialize(part_bytes)?);
+            remaining_data = &remaining_data[(2+part_size)..];
+        }
+        Ok(Self { name, parts })
+    }
 }
 
 struct SongEditor {
@@ -232,7 +454,7 @@ struct SongEditor {
 
 impl SongEditor {
     fn new() -> Self {
-        SongEditor { loaded_songs: Vec::new() }
+        SongEditor { loaded_songs: vec![Song::default()] }
     }
 
     fn ui(&mut self) {
@@ -241,10 +463,11 @@ impl SongEditor {
             self.show_songs_ui();
             println!("Select one of the options listed below:");
             println!("\t1. Load Song");
-            println!("\t2. Create Song");
-            println!("\t3. Edit Song");
-            println!("\t4. Export Song to wav file");
-            println!("\t5. Exit Song Maker");
+            println!("\t2. Save Song");
+            println!("\t3. Create Song");
+            println!("\t4. Edit Song");
+            println!("\t5. Export Song to wav file");
+            println!("\t6. Exit Song Maker");
             let mut option = String::new();
             loop {
                 io::stdin().read_line(&mut option).expect("Could not read user input");
@@ -254,18 +477,22 @@ impl SongEditor {
                         break;
                     }
                     "2" => {
-                        self.create_ui();
+                        self.save_ui();
                         break;
                     }
                     "3" => {
-                        self.edit_ui();
+                        self.create_ui();
                         break;
                     }
                     "4" => {
+                        self.edit_ui();
+                        break;
+                    }
+                    "5" => {
                         self.compile_ui();
                         break;
                     }
-                    "5" | "q" | "Q" => {
+                    "6" | "q" | "Q" => {
                         break 'ui;
                     }
                     _ => {
@@ -317,6 +544,7 @@ impl SongEditor {
             if let Ok(index) = edit_index.parse::<usize>() {
                 if let Some(song) = self.loaded_songs.get(index) {
                     // Editing Song ui
+
                     // TODO
                     println!("Editing Song...");
                     println!("Done editing Song!");
@@ -328,24 +556,72 @@ impl SongEditor {
     }
 
     fn load_ui(&mut self) {
-        // TODO
+        // Get file from the user
+        println!("Select a .song file to load");
+        // Show file dialog
+        let file = FileDialog::new()
+            .add_filter("songs", &["song"])
+            .set_directory("/")
+            .pick_file();
+        match file {
+            Some(file_path) => {
+                if let Ok(f) = File::open(&file_path) {
+                    let mut buf_reader = BufReader::new(f);
+                    let mut serialized_data: Vec<u8> = Vec::new();
+                    if let Err(_err) = buf_reader.read_to_end(&mut serialized_data) {
+                        println!("could not read from file!");
+                        return;
+                    }
+
+                    match Song::deserialize(&serialized_data) {
+                        Ok(song) => {
+                            self.loaded_songs.push(song);
+                            println!("Loaded Song!");
+                            return;
+                        }
+                        Err(err) => {
+                            println!("{err}");
+                        }
+                    }
+                }
+            }
+            None => {
+                println!("No files selected or file failed to open!");
+                return;
+            }
+        }
     }
 
-    fn save(&self) {
-        // TODO
+    fn save_ui(&self) {
+        loop {
+            println!("Which song would you like to save?");
+            let mut save_index = String::new();
+            io::stdin().read_line(&mut save_index).expect("Failed to read user input!");
+            let save_index = save_index.trim();
+            if let Ok(index) = save_index.parse::<usize>() {
+                if let Some(song) = self.loaded_songs.get(index) {
+                    println!("saving song...");
+                    song.write_to_song_file(song.name.clone());
+                    println!("saving complete!");
+                    break
+                }
+            }
+            println!("{save_index} is not a valid song index!")
+        } 
     }
 
     fn compile_ui(&self) {
         loop {
             println!("Which song would you like to compile?");
-            let mut compile_index = String::new();
+            let mut compile_index: String = String::new();
             io::stdin().read_line(&mut compile_index).expect("Failed to read user input!");
             let compile_index = compile_index.trim();
             if let Ok(index) = compile_index.parse::<usize>() {
                 if let Some(song) = self.loaded_songs.get(index) {
-                    println!("compiling song...");
-                    song.write_to_file(song.name.as_str(), &WavOptions::default());
-                    println!("compilation_complete!");
+                    println!("Compiling song...");
+                    // TODO get wavoptions from user optionally
+                    song.write_to_wav_file(song.name.clone(), &WavOptions::default());
+                    println!("Compilation complete!");
                     break
                 }
             }
@@ -355,17 +631,6 @@ impl SongEditor {
 }
 
 fn main() {
-    // // get file options
-    // let wav_options = wavoptions::default();
-
-    // // load / create song
-    // let song = song::new();
-
-    // // create wav file
-    // song.write_to_file("output", &wav_options);
-
-    // // signal completion
-    // print!("created wav file")
     let mut song_editor = SongEditor::new();
     song_editor.ui()
 }
