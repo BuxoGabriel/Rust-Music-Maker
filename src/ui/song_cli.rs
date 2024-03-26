c7185e2ff418007a1c766d3ae54ced3799ae2cf2
Gabriel Buxo <gabrielbuxo1@gmail.com>
2024-03-26 14:43:37 -0400

    added ability to change song names

--- src/ui/song_cli.rs
+++ src/ui/song_cli.rs
@@ -58,7 +58,17 @@ fn add_part_ui(song: &mut Song) {
 }
 
 fn delete_part_ui(song: &mut Song) {
-    todo!()
+    println!("Which part would you like to delete?");
+    let mut buf = String::new();
+    if let Err(_err) = io::stdin().read_line(&mut buf) {
+        println!("Failed to read user input");
+    }
+    let delete_index = buf.trim();
+    if let Ok(part_index) = delete_index.parse::<usize>() {
+        song.parts.remove(part_index);
+    } else {
+        println!("Failed to parse user input as number!");
+    }
 }
 
 fn edit_part_ui(song: &mut Song) {
@@ -80,7 +90,14 @@ fn edit_part_ui(song: &mut Song) {
 }
 
 fn change_name_ui(song: &mut Song) {
-    todo!()
+    // Get new Song name from user
+    print!("New song name: ");
+    io::stdout().flush().expect("Failed to flush stdout! Exiting!");
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).expect("Failed to read user input!");
+    let old_name = song.name.clone();
+    song.name = buf.trim().to_string();
+    println!("Changed name from {old_name} to {}!", song.name);
 }
 
 fn change_bpm_ui(song: &mut Song) {
